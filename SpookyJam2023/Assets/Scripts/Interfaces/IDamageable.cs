e22503bd16657baec2948903daffb2f9fd40d118
Sadrmm <adri2001mm@gmail.com>
2023-10-28 00:28:33 +0200

    Events for IDamageable

--- SpookyJam2023/Assets/Scripts/Interfaces/IDamageable.cs
+++ SpookyJam2023/Assets/Scripts/Interfaces/IDamageable.cs
@@ -1,5 +1,10 @@
+using UnityEngine.Events;
+
 public interface IDamageable
 {
+    public UnityAction<float> OnHealthChanged { get; set; }
+    public UnityAction OnDead { get; set; }
+
     public void Damage(int damage);
     public void Dead();
 }
