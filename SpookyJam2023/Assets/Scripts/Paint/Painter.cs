4c1d63a8ead06fefff354c8faabda19ba475b9db
VizCo1 <al394491@uji.es>
2023-10-28 12:43:30 +0200

    PaintPercentage optimized

--- SpookyJam2023/Assets/Scripts/Paint/Painter.cs
+++ SpookyJam2023/Assets/Scripts/Paint/Painter.cs
@@ -1,35 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.Burst.CompilerServices;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Painter : MonoBehaviour
 {
-
+    [SerializeField] private bool calculate = false;
     [SerializeField] private Color _paintColor;
     [SerializeField] private float _radius = 1;
     [SerializeField] private float _strength = 1;
     [SerializeField] private float _hardness = 1;
 
+    private static int _maskPixels;
+
+    private void Start()
+    {
+        //_maskPixels = 
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Ray ray = new Ray(transform.position + Vector3.up, Vector3.down);
-            RaycastHit hit;
+        Vector3 dir = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            dir += new Vector3(0, 0, 1);
+        if (Input.GetKey(KeyCode.S))
+            dir += new Vector3(0, 0, -1);
+        if (Input.GetKey(KeyCode.A))
+            dir += new Vector3(-1, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+            dir += new Vector3(1, 0, 0);
 
-            if (Physics.Raycast(ray, out hit, 100.0f))
+        dir.Normalize();
+
+        transform.Translate(dir * 10 * Time.deltaTime, Space.Self);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Paint();
+        }
+
+    }
+
+    public void Paint()
+    {
+        Ray ray = new Ray(transform.position + Vector3.up, Vector3.down);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
+            transform.position = hit.point;
+            Paintable p = hit.collider.GetComponent<Paintable>();
+            if (p != null)
             {
-                Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
-                transform.position = hit.point;
-                Paintable p = hit.collider.GetComponent<Paintable>();
-                if (p != null)
+                PaintManager.instance.paint(p, hit.point, _radius, _hardness, _strength, _paintColor);
+                if (calculate)
                 {
-                    PaintManager.instance.paint(p, hit.point, _radius, _hardness, _strength, _paintColor);
+                    ThreadStart threadStart = delegate
+                    {
+                        PaintPercentageController.CalculatePaintedPercentage(p);
+                    };
+                    threadStart.Invoke();
                 }
             }
         }
-
     }
 }
